d8e82d4866307bcc6f8941d3f04b120ae32a8e8c
U_M26M1 <igusev2@alfabank.ru>
2025-09-21 13:49:56 +0300

    Add new devDependencies and update `package-lock.json`

--- src/bot.ts
+++ src/bot.ts
@@ -1,14 +1,15 @@
 import { Bot } from "grammy";
 import { welcome } from "./operations/welcome.js";
+import saveMessage from "./middlewares/saveMessage.js";
 
 const bot = new Bot("1169135083:AAEB4aWHBo6FHa3F3-qNvMmQczPYhPF2dME");
 
+bot.use(saveMessage);
+
 bot.command("start", welcome);
-bot.use((ctx, next) => next());
 
 bot.on("message", (ctx) => {
     ctx.reply(ctx.message.text);
-    console.log(ctx.message.text);
 });
 
 bot.start()
---
+++ src/middlewares/saveMessage.ts
@@ -0,0 +1,24 @@
+import { Context, NextFunction } from "grammy";
+import { ApiConfig, SaturnClient, SaveMessageDto} from "../httpClients/SaturnClient.js";
+
+export default async function saveMessage(ctx: Context, next: NextFunction) {
+    const client = new SaturnClient<ApiConfig>({
+        baseUrl: "http://localhost:5000",
+    });
+    const saveMessageDto: SaveMessageDto = {
+        messageId: ctx.message?.message_id,
+        messageText: ctx.message?.text || null,
+        messageDate: ctx.message?.date ? new Date(ctx.message.date * 1000).toISOString() : undefined,
+        messageType: 1,
+        chatId: ctx.chat?.id,
+        chatName: ctx.chat?.title || ctx.chat?.first_name || null,
+        chatType: 1,
+        stickerFileId: ctx.message?.sticker?.file_id || null,
+        fromId: ctx.from?.id,
+        fromFirstName: ctx.from?.first_name || null,
+        fromLastName: ctx.from?.last_name || null,
+        fromUsername: ctx.from?.username || null,
+    };
+    await client.message.saveMessage(saveMessageDto);
+    next();
+}
